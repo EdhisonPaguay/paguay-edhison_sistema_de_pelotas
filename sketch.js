457cf1d03eb63f6721c8f6ac22c2064673502a89
EdhisonPaguay <edhison.paguay@ucuenca.edu.ec>
2023-10-12 15:56:17 -0500

    preuba dibujo cuadrados

--- sketch.js
+++ sketch.js
@@ -1,13 +1,19 @@
-let miPelota;
-let frutas = ["manzana", "pera", "piñas"];
 function setup() {
   createCanvas(windowWidth, windowHeight);
-  miPelota = new Pelota();
+  noLoop();
+  rectMode(CENTER);
 }
 
 function draw() {
-  background(0, 200, 150, 70);
-  circle(mouseX, mouseY, 20);
-  miPelota.update(300);
-  miPelota.display();
+  background(10, 10, 10);
+  noStroke();
+  let anchura = 20;
+  let gap = random(0, 50);
+
+  for (let x = 50; x < windowWidth; x += anchura + gap) {
+    for (let y = 50; y < windowHeight; y += anchura + gap) {
+      fill(47, 255, 255);
+      square(x, y, anchura);
+    }
+  }
 }
